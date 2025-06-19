8b0b51159456ffed1635ddaab5c02fccbf037422
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-06-19 01:59:45 -0500

    feat(UPS-00): Se agrego todo items en en main y solo falta hacer que aparescan mas carritos

---
+++ src/main/java/edu/ec/ups/dao/CarritoDAO.java
@@ -0,0 +1,19 @@
+package edu.ec.ups.dao;
+
+import edu.ec.ups.modelo.Carrito;
+
+import java.util.List;
+
+public interface CarritoDAO {
+
+    void crear(Carrito carrito);
+
+    Carrito buscarPorCodigo(int codigo);
+
+    void actualizar(Carrito carrito);
+
+    void eliminar(int codigo);
+
+    List<Carrito> listarTodos();
+
+}
