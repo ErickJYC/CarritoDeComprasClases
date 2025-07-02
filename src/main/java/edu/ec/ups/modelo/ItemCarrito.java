2b97a34abe47e4b45a7455171a2e359f7b3dd073
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-07-02 16:32:37 -0500

    fix (UPS-025): Se modifico el ItemCarrito

--- src/main/java/edu/ec/ups/modelo/ItemCarrito.java
+++ src/main/java/edu/ec/ups/modelo/ItemCarrito.java
@@ -12,27 +12,35 @@ public class ItemCarrito {
         this.cantidad = cantidad;
     }
 
-    public Producto getProducto() {
-        return producto;
-    }
-
     public void setProducto(Producto producto) {
         this.producto = producto;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
     }
 
     public int getCantidad() {
         return cantidad;
     }
 
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
     public double getSubtotal() {
         return producto.getPrecio() * cantidad;
+    }
+    public double getIVA() {
+        return getSubtotal() * 0.12; // Asumiendo un IVA del 12%
+    }
+    public double getTotal() {
+        return getSubtotal() + getIVA();
     }
 
     @Override
     public String toString() {
         return producto.toString() + " x " + cantidad + " = $" + getSubtotal();
     }
+
 }
